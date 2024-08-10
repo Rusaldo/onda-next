2e702a71445fe58d858daec149fa09355268ff3e
Ruslan Yandiev <yandiev_97@mail.ru>
2024-08-10 16:02:06 +0300

    скрыл дату статьи

--- src/components/HomeArticles.jsx
+++ src/components/HomeArticles.jsx
@@ -29,9 +29,9 @@ function Article({ article }) {
       <Card.Title href={`/articles/${article.slug}`}>
         {article.title}
       </Card.Title>
-      <Card.Eyebrow as="time" dateTime={article.date} decorate>
+      {/* <Card.Eyebrow as="time" dateTime={article.date} decorate>
         {formatDate(article.date)}
-      </Card.Eyebrow>
+      </Card.Eyebrow> */}
       <Card.Description>{article.description}</Card.Description>
       <Card.Cta>Читать далее</Card.Cta>
     </Card>
